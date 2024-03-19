4a8a0f5fb48a7013dff21287f3a7d926b5ca2874
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-19 14:35:30 +0000

    Add dice generated with seed

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,9 +6,21 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
+            int result = 0;
             string str1 = args[0];
             int diceNum = int.Parse(str1);
-            
+
+            string str2 = args[1];
+            int seed = int.Parse(str2);
+
+
+            Random dice = new Random(seed);
+
+            for (int i = 0; i < diceNum; i++)
+            {
+                result += dice.Next(1, 7);
+            }
+            Console.WriteLine(result);
         }
     }
 }
