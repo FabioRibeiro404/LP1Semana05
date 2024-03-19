8fd7142e4b32f84236ee14ece6010fcc4671b396
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-19 14:25:48 +0000

    Add args

---
+++ RanDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = args[0];
+            int diceNum = int.Parse(str1);
+            
+        }
+    }
+}
