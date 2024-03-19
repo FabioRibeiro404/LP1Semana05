ed550991e3e8d9034c199feb89aa117899072dc3
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-19 14:14:49 +0000

    Add timer for first chrono

---
+++ Chronos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch chrono1 = new Stopwatch();
+            Stopwatch chrono2 = new Stopwatch();
+
+            chrono1.Start();
+            Thread.Sleep(600);
+            chrono2.Start();
+            Thread.Sleep(200);
+
+            chrono1.Stop();
+            TimeSpan ts1 = chrono1.Elapsed;
+
+            chrono2.Stop();
+            TimeSpan ts2 = chrono2.Elapsed;
+
+            Console.WriteLine($"Time 1: {ts1.Seconds},{ts1.Milliseconds:d3}");
+
+            //Console.WriteLine($"Time 2: {ts2:f3}");
+        }
+    }
+}
