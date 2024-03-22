43faf495fccdb934f06e9eab9d875160b478d409
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-22 13:20:56 +0000

    Add new program and Enemy

---
+++ MyGameFriday/Enemy.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGameFriday
+{
+    public class Enemy
+    {
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+    }
+}
